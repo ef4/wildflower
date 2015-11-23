b6625597079c741c12253c29ce0bda15a9e8b0c4
Edward Faulkner <ef@alum.mit.edu>
2015-11-23 01:42:49 -0500

    declarative spreadsheet config

--- app/components/wf-budget/component.js
+++ app/components/wf-budget/component.js
@@ -1,3 +1,13 @@
+export const budgetConfig = [
+  {
+    onSheet: 'START UP COST',
+    do: [
+      { put: 'salaries' , into: 'B7' },
+      { put: 'rent' , into: 'B11' }
+    ]
+  }
+];
+
 /* global saveAs */
 import Ember from 'ember';
 
@@ -5,9 +15,12 @@ export default Ember.Component.extend({
   actions: {
     create() {
       let budget = this.get('budget');
-      budget.sheet('START UP COST')
-        .write('B7', this.get('salaries'))
-        .write('B11', this.get('rent'));
+      budgetConfig.forEach(sheetEntry => {
+        let sheet = budget.sheet(sheetEntry.onSheet);
+        sheetEntry.do.forEach(edit => {
+          sheet.write(edit.into, this.get(edit.put));
+        });
+      });
       saveAs(budget.asBlob(), 'wildflower-school-budget.xlsx');
     }
   }
