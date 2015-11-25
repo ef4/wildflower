8bc478c366ff4bf48492abbbefa5d49af00cae83
Edward Faulkner <ef@alum.mit.edu>
2015-11-25 17:37:35 -0500

    absorbed Rick's markup

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,5 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {});
   app.import('vendor/FileSaver.js');
   app.import('vendor/jszip.js');
-  app.import('vendor/custom-bootstrap.css');
   return app.toTree();
 };
