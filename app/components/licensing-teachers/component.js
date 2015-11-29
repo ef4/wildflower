50f325b54a1774a4a632773288b5084eeafe3e53
Edward Faulkner <ef@alum.mit.edu>
2015-11-29 16:28:25 -0500

    toward dynamic answers

--- app/components/licensing-teachers/component.js
+++ app/components/licensing-teachers/component.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  haveAnswer: Ember.computed('model.ageRange', 'model.classSize', function() {
+  filledIn: Ember.computed('model.ageRange', 'model.classSize', function() {
     return this.get('model.ageRange') && this.get('model.classSize');
+  }),
+  answer: Ember.computed('model.ageRange', 'model.classSize', function() {
+    // hard coded for now
+    return {};
   }),
   actions: {
     print() {
