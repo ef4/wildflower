6e77e84efd3cd89037df815d769c8ccb3c9b37d7
Edward Faulkner <ef@alum.mit.edu>
2015-12-04 00:16:57 -0500

    header changes

--- app/routes/budget.js
+++ app/routes/budget.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  subHeader: Ember.inject.service(),
-
-  beforeModel() {
-    this.get('subHeader').set('title', 'Budget');
-  },
-
   model() {
     return this.store.findRecord('budget', 'template');
   }
--- app/routes/licensing.js
+++ app/routes/licensing.js
@@ -1,14 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  subHeader: Ember.inject.service(),
-
-  beforeModel() {
-    this.get('subHeader').set('title', 'Licensing');
-  },
-
   model() {
     return this.store.findAll('regulation');
   }
-
 });
